66cfcd8b101d823d859f8457605935045b0b0938
Hcode93 <hcode93@gmail.com>
2022-07-11 14:52:39 -0500

    “commit_11-jul-22-14:52”

--- app.js
+++ app.js
@@ -27,6 +27,87 @@ const Samborondon = "https://www.controlsanitario.gob.ec/wp-content/uploads/down
 const Duran = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ8-TURNOS-DE-FARMACIAS-DURAN-JULIO-2022.pdf";
 const Quito = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/TURNOFARMACIA-CZ9-QUITO-JULIO-2022.pdf";
 /* --------- ----------- */
-function EsmeraldasView(){
+function Esmeraldas_View(){
     window.open(Esmeraldas,"_blank");
+}
+function Imbabura_View(){
+    window.open(Imbabura,"_blank");
+}
+function Carchi_View(){
+    window.open(Carchi,"_blank");
+}
+function Sucumbios_View(){
+    window.open(Sucumbios,"_blank");
+}
+function Pichincha_View(){
+    window.open(Pichincha,"_blank");
+}
+function Napo_View(){
+    window.open(Napo,"_blank");
+}
+function Orellana_View(){
+    window.open(Orellana,"_blank");
+}
+function Cotopaxi_View(){
+    window.open(Cotopaxi,"_blank");
+}
+function Tungurahua_View(){
+    window.open(Tungurahua,"_blank");
+}
+function Chimborazo_View(){
+    window.open(Chimborazo,"_blank");
+}
+function Pastaza_View(){
+    window.open(Pastaza,"_blank");
+}
+function Manabi_View(){
+    window.open(Manabi,"_blank");
+}
+function SantoDomingo_View(){
+    window.open(SantoDomingo,"_blank");
+}
+function SantaElena_View(){
+    window.open(SantaElena,"_blank");
+}
+function Bolivar_View(){
+    window.open(Bolivar,"_blank");
+}
+function LosRios_View(){
+    window.open(LosRios,"_blank");
+}
+function Galapagos_View(){
+    window.open(Galapagos,"_blank");
+}
+function Guayas_View(){
+    window.open(Guayas,"_blank");
+}
+function Cañar_View(){
+    window.open(Cañar,"_blank");
+}
+function Azuay_View(){
+    window.open(Azuay,"_blank");
+}
+function MoronaSantiago_View(){
+    window.open(MoronaSantiago,"_blank");
+}
+function ElOro_View(){
+    window.open(ElOro,"_blank");
+}
+function Loja_View(){
+    window.open(Loja,"_blank");
+}
+function ZamoraChinchipe_View(){
+    window.open(ZamoraChinchipe,"_blank");
+}
+function Guayaquil_View(){
+    window.open(Guayaquil,"_blank");
+}
+function Samborondon_View(){
+    window.open(Samborondon,"_blank");
+}
+function Duran_View(){
+    window.open(Duran,"_blank");
+}
+function Quito_View(){
+    window.open(Quito,"_blank");
 }
