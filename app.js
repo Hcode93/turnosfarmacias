09ed57e95254a378a4e3154da020189d2a29748a
Hcode93 <hcode93@gmail.com>
2022-07-11 10:58:57 -0500

    “commit_11-jul-22-10:58”

---
+++ app.js
@@ -0,0 +1,32 @@
+const Esmeraldas = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ1-TURNOS-DE-FARMACIA-ESMERALDAS-JULIO-2022.pdf";
+const Imbabura = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ1-TURNOS-DE-FARMACIA-IMBABURA-JULIO-2022.pdf";
+const Carchi = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ1-TURNOS-DE-FARMACIA-CARCHI-JULIO-2022.pdf";
+const Sucumbios = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ1-TURNOS-DE-FARMACIA-SUCUMBIOS-JULIO-2022.pdf";
+const Pichincha = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/CZ2-TURNOS-DE-FARMACIA-JULIO-2022-PICHINCHA-RURAL.pdf";
+const Napo = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/CZ2-TURNOS-DE-FARMAICA-JULIO-2022-NAPO.pdf";
+const Orellana = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/CZ2-TURNOS-DE-FARMACIA-JULIO-2022-ORELLANA.pdf";
+const Cotopaxi = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/ARCSA-CZ3-TURNOS-DE-FARMACIAS-COTOPAXI-JULIO-2022.pdf";
+const Tungurahua = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/ARCSA-CZ3-TURNOS-DE-FARMACIAS-TUNGURAHUA-JULIO-2022.pdf";
+const Chimborazo = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/ARCSA-CZ3-TURNOS-DE-FARMACIAS-CHIMBORAZO-JULIO-2022.pdf";
+const Pastaza = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/ARCSA-CZ3-TURNOS-DE-FARMACIAS-PASTAZA-JULIO-2022.pdf";
+const Manabi = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/Arcsa_CZ4_Turnos-de-farmacia_Manabi_Julio_2022.pdf";
+const SantoDomingo = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/Arcsa_CZ4_Turnos-de-farmacia_Santo-Domingo_Julio_2022.pdf";
+const SantaElena = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/ARCSA-CZ5-TURNOS-DE-FARMACIAS-SANTA-ELENA-JULIO-2022.pdf";
+const Bolivar = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/ARCSA-CZ5-TURNOS-DE-FARMACIAS-BOLIVAR-JULIO-2022.pdf";
+const LosRios = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/ARCSA-CZ5-TURNOS-DE-FARMACIAS-LOS-RIOS-JULIO-2022.pdf";
+const Galapagos = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/ARCSA-CZ5-TURNOS-DE-FARMACIAS-GALAPAGOS-JULIO-2022.pdf";
+const Guayas = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/06/ARCSA-CZ5-TURNOS-DE-FARMACIAS-GUAYAS-JULIO-2022.pdf";
+const Cañar = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/7-ARCSA-CZ6-Turno-de-farmacia-CANAR-jul.pdf";
+const Azuay = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/7.-ARCSA-CZ6-Turno-de-farmacia-AZUAY-jul.pdf";
+const MoronaSantiago = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/7.-ARCSA-CZ6-TURNOS-DE-FARMACIA-MS-Jul.pdf";
+const ElOro = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ7-TURNOS-DE-FARMACIAS-ELORO-JULIO-2022.pdf";
+const Loja = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ7-TURNOS-DE-FARMACIAS-LOJA-JULIO-2022.pdf";
+const ZamoraChinchipe = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ7-TURNOS-DE-FARMACIAS-ZAMORA-JULIO-2022.pdf";
+const Guayaquil = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ8-TURNOS-DE-FARMACIAS-GUAYAQUIL-JULIO-2022.pdf";
+const Samborondon = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ8-TURNOS-DE-FARMACIAS-SAMBORONDON-JULIO-2022.pdf";
+const Duran = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/ARCSA-CZ8-TURNOS-DE-FARMACIAS-DURAN-JULIO-2022.pdf";
+const Quito = "https://www.controlsanitario.gob.ec/wp-content/uploads/downloads/2022/07/TURNOFARMACIA-CZ9-QUITO-JULIO-2022.pdf";
+/* --------- ----------- */
+function EsmeraldasView(){
+    window.open(Esmeraldas,"_blank");
+}
